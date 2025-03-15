30f3c98970660d4086047bbf9df27ed46f25f83e
Vitor Mimaki <vitormimaki@gmail.com>
2025-03-14 21:37:13 -0300

    Final

--- src/app/index.js
+++ src/app/index.js
@@ -17,11 +17,11 @@ export default class index extends Component {
   state = {...initialState}
 
   addDigit = n => {
-    if(n === '.' && this.state.displayValue.includes('.')) {
+    
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    if(n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
       return
     }
-
-    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
     const current = clearDisplay ? '' : this.state.displayValue
     const displayValue = current + n
     this.setState({displayValue, clearDisplay: false})
@@ -52,7 +52,7 @@ export default class index extends Component {
 
       values[1] = 0
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         //clearDisplay: !equals,
